033f262c90d4e852f0f3c2122e3cafbeaaa7e19b
aanchal malhotra <amalhotra0493@gmail.com>
2018-12-31 15:18:25 +0530

    Adding download code

--- content.js
+++ content.js
@@ -18,20 +18,21 @@
 		let elem = document.createElement("img");
 		elem.setAttribute("src", src[i]);
 		elem.class = "img_chrome";
-		let link = document.createElement('a');
-		link.id = "linkDiv_" + i;	
+		elem.title = "Click to download";
+		//link.id = "linkDiv_" + i;	
 		//elem.setAttribute("width", "100");
 		document.getElementById("chrome_app").appendChild(outerDiv);
-		document.getElementById(outerDiv.id).appendChild(link);	
-		document.getElementById(link.id).appendChild(elem);
-		$('#' + link.id).data("download" , "image" + i);
-		$('#' + link.id).data("href" , src[i]);
+		document.getElementById(outerDiv.id).appendChild(elem);	
+		//document.getElementById(link.id).appendChild(elem);
+		$('#' + outerDiv.id).data("download" , "image" + i);
+		$('#' + outerDiv.id).data("href" , src[i]);
 
-		$('#' + link.id).click(function() {
-  			
-  			this.download = $(this).data("download");
- 			this.href = $(this).data("href");
- 			this.click();
+		$('#' + outerDiv.id).click(function(event) {			
+			let link = document.createElement('a');
+  			link.target = "_self"
+  			link.download = $(this).data("download");
+ 			link.href = $(this).data("href");
+ 			link.click();
 		});
 	}
 
